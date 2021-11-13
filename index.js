17991e2268b0c135bcd5b13539ebfd2882303814
Denis <msden.ua@gmail.com>
2021-11-13 23:47:19 +0200

    define sum function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 //const x = require('./module-example/index.js')
+const sum = ar => ar.reduce((a, i) => a + i, 0)
 const x = {a:1*2+3,b:"Hello world!", f:x=>x*x}
-module.exports = {a:x.a*2,b:x.b,c:x.f(7)}
+module.exports = {a:x.a*2,b:x.b,c:x.f(7),d:sum([1, 2, 3])}
