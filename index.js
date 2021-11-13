0d6bb2e856bf08f06d13794fbcb1116561eb2f36
Denis <msden.ua@gmail.com>
2021-11-13 23:36:56 +0200

    fix function call

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 //const x = require('./module-example/index.js')
 const x = {"a":1*2+3,"b":"Hello world!", "f":x=>x*x}
-module.exports = {"a":x.a*2,"b":x.b,"c":f(7)}
+module.exports = {"a":x.a*2,"b":x.b,"c":x.f(7)}
