b9c25e190610c8ba52974354364e9b315c4106f6
Denis <msden.ua@gmail.com>
2021-11-13 23:40:15 +0200

    remove quotes

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 //const x = require('./module-example/index.js')
-const x = {"a":1*2+3,"b":"Hello world!", "f":x=>x*x}
-module.exports = {"a":x.a*2,"b":x.b,"c":x.f(7)}
+const x = {a:1*2+3,b:"Hello world!", f:x=>x*x}
+module.exports = {a:x.a*2,b:x.b,c:x.f(7)}
